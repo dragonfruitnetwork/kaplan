52a6401bd6ef6616e2f126deb860ae724f8bc6b7
Albie <albion@dragonfruit.network>
2023-05-19 19:47:53 +0100

    move assets and add license.txt

--- DragonFruit.Kaplan/Views/About.axaml.cs
+++ DragonFruit.Kaplan/Views/About.axaml.cs
@@ -1,7 +1,12 @@
 // Kaplan Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
 // Licensed under Apache-2. Refer to the LICENSE file for more info
 
+using System;
+using System.IO;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform;
+using Avalonia.Threading;
 
 namespace DragonFruit.Kaplan.Views
 {
@@ -10,6 +15,15 @@ namespace DragonFruit.Kaplan.Views
         public About()
         {
             InitializeComponent();
+        }
+
+        private async void OnLoad(object sender, RoutedEventArgs e)
+        {
+            using var licenseFileStream = AssetLoader.Open(new Uri("avares://DragonFruit.Kaplan/Assets/licences.txt"));
+            using var streamReader = new StreamReader(licenseFileStream);
+
+            var contents = await streamReader.ReadToEndAsync().ConfigureAwait(false);
+            Dispatcher.UIThread.InvokeAsync(() => LicenseContents.Text = contents);
         }
     }
 }
