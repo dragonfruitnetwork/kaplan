4492f44a6df5db12121f06aadc43d00ce1605b63
Albie Spriddell <albion@dragonfruit.network>
2023-07-03 14:37:09 +0100

    improve progress display

--- DragonFruit.Kaplan/ViewModels/PackageRemovalTask.cs
+++ DragonFruit.Kaplan/ViewModels/PackageRemovalTask.cs
@@ -29,8 +29,9 @@ namespace DragonFruit.Kaplan.ViewModels
             _statusString = this.WhenAnyValue(x => x.Progress)
                 .Select(x => x?.state switch
                 {
-                    DeploymentProgressState.Queued => $"Removing {Package.Name}: Queued",
-                    DeploymentProgressState.Processing => $"Removing {Package.Name}: {x.Value.percentage}% Complete",
+                    DeploymentProgressState.Queued => $"Removing {Package.Name}: Pending",
+                    DeploymentProgressState.Processing when x.Value.percentage == 100 => $"Removing {Package.Name} Complete",
+                    DeploymentProgressState.Processing when x.Value.percentage > 0 => $"Removing {Package.Name}: {x.Value.percentage}% Complete",
 
                     _ => $"Removing {Package.Name}"
                 })
