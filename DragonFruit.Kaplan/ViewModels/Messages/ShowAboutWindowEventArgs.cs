afa1a1c988373344f99e45d69c8b97ec0cec7451
Albie Spriddell <albion@dragonfruit.network>
2024-07-05 11:40:07 +0100

    shut down messagebus

--- DragonFruit.Kaplan/ViewModels/Messages/ShowAboutWindowEventArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿// Kaplan Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
-// Licensed under Apache-2. Refer to the LICENSE file for more info
-
-namespace DragonFruit.Kaplan.ViewModels.Messages
-{
-    public class ShowAboutWindowEventArgs
-    {
-    }
-}
